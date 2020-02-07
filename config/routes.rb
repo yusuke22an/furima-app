2edcf5fa2ef5906e299208822c841f783c995b78
yusuke22an <yu202.sin702@gmail.com>
2020-02-07 13:12:55 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'furima/index'
-  get 'furima/index'
+  root 'furima#index'
+  # get 'furima/index'
+
+  resources :furima
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
