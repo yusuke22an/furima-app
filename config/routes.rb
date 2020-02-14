ecc9c2dad6ac32459777657deca757990b410884
ryusuketake <tkd09043625429@icloud.com>
2020-02-14 22:43:39 +0900

    user_seibi_tocyuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
+  resources :items, only: :index
   resources :comments
+  
+
   root 'items#index'
-  resources :furima
+
+  # resources :furima
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
