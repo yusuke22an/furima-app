d2664c188c4a297bbdb5788d8805d410cf1225f2
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-02-23 23:54:44 +0900

    画像投稿機能の実装
    itemコントローラーにnewとcreateを作成
    actrivestrageを使用する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+# 商品出品用のアクション
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create params.require(:item).permit(:name, :text, :status, :shipping_charges, :shipping_area, :days_to_ship, :price).merge(seller_id: current_user.id)
+    redirect_to root_path
+  end
+
+# 商品購入機能用のアクション（仮）
   def update
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   # belongs_to :category
   belongs_to :saler
   belongs_to :buyer
+  has_many_attached :photos, dependent: :destroy
+  # accepts_nested_attributes_for :photos
 end
