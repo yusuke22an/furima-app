88f4d8573cd1041d367f124e823763f4c7c10db4
yusuke22an <yu202.sin702@gmail.com>
2020-02-23 14:40:20 +0900

    Cardモデルの作成

---
+++ db/migrate/20200222114453_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.references :user, foreign_key: true, null: false
+      # t.integer :user_id
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
+
+      t.timestamps
+    end
+  end
+end
