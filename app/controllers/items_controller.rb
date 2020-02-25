cdb95a0bead788d7f1967d788e0cb04a707f6794
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-02-25 19:43:08 +0900

    前回ファイル作成途中の写しかえ
    作成途中だった商品投稿に関するファイルを再度反映。
    ・active_strage使用のためのモデルファイルの追記
    ・itemsコントローラーに今回必要なアクションを追記
    ・フロントに画像投稿フォームを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,18 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def update
+  # 商品出品用のアクション
+  def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.create params.require(:item).permit(:name, :text, :status, :shipping_charges, :shipping_area, :days_to_ship, :price).merge(seller_id: current_user.id)
+    redirect_to root_path
+  end
+
+  # 商品購入機能用のアクション（仮）
+  def update
+end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
-  # has_many :photos, dependent: :destroy
   belongs_to :category
   belongs_to :saler
   belongs_to :buyer
+  has_many_attached :photos, dependent: :destroy
+  # accepts_nested_attributes_for :photos
+
 end
