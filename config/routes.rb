4a7dae8f649d0a7d123b292aa87264fcfcf191c1
yusuke22an <yu202.sin702@gmail.com>
2020-02-27 22:53:04 +0900

    card登録画面、確認画面、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   resources :comments
   root 'items#index'
   resources :furima
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
