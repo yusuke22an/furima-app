aa30771dac23b795816aa1f28f43f59e6568e5d2
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-03-06 20:03:39 +0900

    孫カテゴリーをデータ登録するための記述修正
    ・jsonの記述を修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,7 +1,8 @@
 $(function(){
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category){
-    var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    // var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    var html = `<option value="${category.id}">${category.name}</option>`
     return html;
   }
   // 子カテゴリーの表示作成
@@ -9,8 +10,8 @@ $(function(){
     var childSelectHtml = '';
     childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
                         <div class='listing-select-wrapper__box'>
-                          <select class="listing-select-wrapper__box--select" id="child_category" name="category_id">
-                            <option value="---" data-category="---">---</option>
+                          <select class="listing-select-wrapper__box--select" id="child_category">
+                            <option value="---">---</option>
                             ${insertHTML}
                           <select>
                           
@@ -23,23 +24,26 @@ $(function(){
     var grandchildSelectHtml = '';
     grandchildSelectHtml = `<div class='listing-select-wrapper__added' id= 'grandchildren_wrapper'>
                               <div class='listing-select-wrapper__box'>
-                                <select class="listing-select-wrapper__box--select" id="grandchild_category" name="category_id">
-                                  <option value="---" data-category="---">---</option>
+                                <select class="listing-select-wrapper__box--select" id="grandchild_category" name="item[category_id]">
+                                  <option value="---">---</option>
                                   ${insertHTML}
                                 </select>
                                 
                               </div>
                             </div>`;
+                            console.log(grandchildSelectHtml)
     $('.listing-product-detail__category').append(grandchildSelectHtml);
   }
   // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
+    console.log(parentCategory)
     if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_children',
         type: 'GET',
-        data: { parent_name: parentCategory },
+        // data: { parent_name: parentCategory },
+        data: ('parent_name=' + parentCategory),
         dataType: 'json'
       })
       .done(function(children){
@@ -60,21 +64,26 @@ $(function(){
     }
   });
   // 子カテゴリー選択後のイベント
-  $('.listing-product-detail__category').on('change', '#child_category', function(){
-    var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
+  $(document).on('change', '#child_category', function(){
+    // var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
+    var childId = $(this).val()
+    console.log(childId)
     if (childId != "---"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_grandchildren',
         type: 'GET',
-        data: { child_id: childId },
+        // data: { child_id: childId },
+        data: ('child_id=' + childId),
         dataType: 'json'
       })
       .done(function(grandchildren){
+        console.log(grandchildren)
         if (grandchildren.length != 0) {
           $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除するする
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
+            console.log(grandchild)
           });
           appendGrandchidrenBox(insertHTML);
         }
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,14 +7,15 @@ class ItemsController < ApplicationController
     @items = Item.new
     @category_parent_array = Category.where(ancestry: nil).each do |parent|
     end
+    # @category_parent_array = Category.all.order("id ASC")
   end
 
   def create
     @item = Item.new(create_params)
-      if @item.save!
-        redirect_to controller: :items, action: :index ,notice: "投稿完了しました"
+    if @item.save
+        redirect_to root_path,notice: "投稿完了しました"
       else
-        render :new
+        render :new, notice: "fail"
       end        
   end
   
@@ -23,16 +24,17 @@ class ItemsController < ApplicationController
   end
 
   def get_category_children
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    # @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    @category_children = Category.where('ancestry = ?', "#{params[:parent_name]}")
     # ここの記述を直せば動くと思われる。
   end
 
   def get_category_grandchildren
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
+    @category_grandchildren = Category.where('ancestry LIKE ?', "%/#{params[:child_id]}")
   end
 
 private
   def create_params
-    params.require(:item).permit(:name, :text, :status, :shipping_charges, :shipping_area, :days_to_ship, :price, category_id:[], photos:[]).merge(saler_id: current_user.id)
+    params.require(:item).permit(:name, :text, :category_id, :brand_name, :status, :shipping_charges, :shipping_area, :days_to_ship, :price, photos:[]).merge(saler_id: current_user.id)
   end
 end
