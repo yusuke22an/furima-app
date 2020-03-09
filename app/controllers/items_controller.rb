2398bda0bb0bbfc0ebfcda274d26452ffcf428e8
ryusuketake <tkd09043625429@icloud.com>
2020-03-10 00:04:25 +0900

    Add_js_top_page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
   
   def show
+    @categories = Category.all
     @user = User.find(current_user.id)
   end
 end
