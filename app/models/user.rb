22df8f3ed2bcd8d5d56b0793a0f020c60c1274d7
ryusuketake <tkd09043625429@icloud.com>
2020-03-30 19:29:49 +0900

    Revert "Merge branch 'Add_Like' of https://github.com/yusuke22an/furima-app into Add_Like"
    This reverts commit cb4716f24c38b6500e9a733b093a48c2339de423, reversing
    changes made to 71f611aa50dd7f6cbbdcbe1582ba6167dbdf696e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :validatable
 
-  has_many :sns_credentials
   has_many :cards 
   has_many :items
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
@@ -35,20 +34,4 @@ class User < ApplicationRecord
   validates :prefectures, presence: true
   validates :municipality, presence: true
   validates :address, presence: true
-
-  def self.from_omniauth(auth)
-    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-    # sns認証したことがあればアソシエーションで取得
-    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
-    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
-      nickname: auth.info.name,
-        email: auth.info.email
-    )
-    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
-    if user.persisted?
-      sns.user = user
-      sns.save
-    end
-    user
-  end
 end
