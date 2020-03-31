71a526c2a9dbc09631b8b9e4ebd40307f517dcd0
yusuke22an <yu202.sin702@gmail.com>
2020-03-31 13:49:19 +0900

    Revert "Merge branch 'master' into OmniAuth"
    This reverts commit ae44ed7b30840d8520bdd3a08ec36ca51cdfb18b, reversing
    changes made to 250ac0118d6986fb808922ab2fab708a4d2ab12d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :users, only: :show 
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  
+
+  resources :users, only: :show
   resources :items do
     get 'buy_item', to: :update, controller: 'items'
     get :like, on: :member
@@ -26,6 +31,5 @@ Rails.application.routes.draw do
     end
   end
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
